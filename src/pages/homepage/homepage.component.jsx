8c86dab5df54985f0dfd680172f31c225d8f866e
elroychibueze <etoniru.chibueze@everyfarmer.farm>
2019-11-11 06:51:42 +0100

    done with header section

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import Directory from "../../components/directory/directory.component";
+import "./homepage.styles.scss";
+
+const Hompage = () => (
+  <div className="homepage">
+    <Directory />
+    {/* <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HEADS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HEARTS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMENS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div> */}
+  </div>
+);
+
+export default Hompage;
