2687889bc91e0282895a36b615f838603fcf8a10
elroychibueze <etoniru.chibueze@everyfarmer.farm>
2019-11-19 18:02:51 +0100

    integrate sign up/sign in / firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyAYzK0lICQzi1hUamgWIGdUc567Lo44-TY",
+  authDomain: "elroy-db.firebaseapp.com",
+  databaseURL: "https://elroy-db.firebaseio.com",
+  projectId: "elroy-db",
+  storageBucket: "elroy-db.appspot.com",
+  messagingSenderId: "101915789693",
+  appId: "1:101915789693:web:cb2c40ac337826d76b6414",
+  measurementId: "G-NGWXXERNQB"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestone = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
